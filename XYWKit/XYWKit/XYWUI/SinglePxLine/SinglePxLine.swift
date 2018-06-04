fcf768d91df9a2759c3044111b5d4ac72fd401e6
xueyongwei <xueyongwei@ylmf.com>
2018-06-04 16:55:30 +0800

    rename

--- XYWKit/XYWKit/XYWUI/SinglePxLine/SinglePxLine.swift
+++ XYWKit/XYWKit/XYWUI/SinglePxLine/SinglePxLine.swift
@@ -11,7 +11,7 @@ import UIKit
 /** XueYongWei 2018.6
  SinglePxHLine：单像素横线
  SinglePxVLine：单像素竖线
- 这两个类保证分割是单像素的分割线。
+ 这两个类保证分割是单像素的分割线，支持代码和IB创建，任意布局方式。
  释疑：继承自UIImageView，且不是通过backgroundColor设置背景色，是因为一般都是在cell中使用分割线，但是cell在选中状态会清空子view的背景色，导致分割线“消失”。故使用了一个带颜色的图片来替代背景色，防止cell选中状态分割线消失的情况发生。
  **/
 
