9e1344ef8dc72418e05d1ecf5d1d35c2bafcb70a
xueyongwei <xueyongwei@ylmf.com>
2018-06-04 15:55:08 +0800

    添加XYWUI
    添加封装的UIAlertViewController

---
+++ XYWKit/XYWKit/XYWUI/SinglePxLine/SinglePxLine.swift
@@ -0,0 +1,20 @@
+//
+//  SinglePxLine.swift
+//  XYWKit
+//
+//  Created by 薛永伟 on 2018/6/4.
+//  Copyright © 2018年 薛永伟. All rights reserved.
+//
+
+import UIKit
+
+/// 单像素横线
+class SinglePxHLine:UIImageView{
+    
+}
+
+/// 单像素竖线
+class SinglePxVLine:UIImageView{
+    
+}
+
